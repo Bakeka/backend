19aee29fc63c1285f5c9c86aae9df8daad6696ad
Pauline <pauline.caldonazzo@studenti.unitn.it>
2022-05-21 14:07:38 +0200

    redefinition of schema, create another file bakeka to insert initial data

--- schema.js
+++ schema.js
@@ -1,59 +1,52 @@
 //schema
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
+//connection with my db
+mongoose.connect('mongodb+srv://admin:ingegneria@cluster0.njhju.mongodb.net/?retryWrites=true&w=majority');
 
+var Schema = mongoose.Schema; 
 
-const bakekaSchema = new Schema({
-    name: String,
-    id: Integer,
-    position: [{ posx: Double, posy: Double}],
-    purpose: String,
+var materialSchema = Schema({
+    typeMaterial: String
+});
+
+var Material = mongoose.model('Material', materialSchema); 
+
+var purposeSchema = Schema({
+    purpose: String
+});
+
+var Purpose = mongoose.model('Purpose', purposeSchema); 
+
+var accessibilitySchema = Schema({
+    accessibility: String
+});
+
+var Accessibility = mongoose.model('Accessibility', accessibilitySchema); 
+
+var affluenceSchema = Schema({
+    affluence: String
+});
+
+var Affluence = mongoose.model('Affluence', affluenceSchema); 
+
+var bakekaSchema = new Schema({   
+    id: Number, 
+    name: String, 
+    //affluences: String,
+    affluence: [{ type:String, ref: 'Affluence'}],
+    //materials: String,
+    material: [{ type:String, ref: 'Material'}],
+    //purposes: String,
+    purpose: [{ type:String, ref: 'Purpose'}],
+    //accessibilty: String,
+    accessibilty: [{ type:String, ref: 'Accessibility'}],
     url: String,
-    material: String,
-    accessibilty: String,
-    affluence: String 
-    
-});
-
-bakekaSchema.path('_id'); // ObjectId { ... }
-
-const bakekaModel = mongoose.model('Bakeka', bakekaSchema);
+    position: {      latitude: Number,      longitude: Number    } 
 
 
+});  
+var Bakeka = mongoose.model('Bakeka', bakekaSchema); 
 
 
+export {Bakeka, Affluence, Accessibility, Material, Purpose};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const bookSchema = new Schema({
-title: String, // String is shorthand for {type: String}
-author: String,
-body: String,
-comments: [{ body: String, date: Date }],
-date: { type: Date, default: Date.now },
-hidden: Boolean,
-meta: {
-votes: Number,
-favs: Number
-}
-});
-
-
-
-
-
-const Cat = mongoose.model('Cat', { name: String }); //interface with a specific collection 
-const kitty = new Cat({ name: 'Zildjian' }); //document 
-kitty.save().then(() => console.log('meow'));
