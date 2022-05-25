aba8f3ea2c05f9823df7df19de8621ecaca7be43
Baldomo <leobaldin.2000@gmail.com>
2022-05-25 13:07:53 +0200

    Removed redoc in favour of Swagger again, added documentation, refined tooling and Docker Compose deployment script

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN yarn && yarn build
 FROM node:18-alpine
 WORKDIR /bakeka
 COPY --from=builder /bakeka/build ./build
-COPY bakeka.config.json .
+COPY . .
 RUN yarn install --frozen-lockfile --no-cache --production
 EXPOSE 8080
 ENTRYPOINT [ "node", "build/app.js", "bakeka.config.json" ]
--- src/entities/Enums.ts
+++ src/entities/Enums.ts
@@ -1,9 +1,26 @@
+/**
+ * Enum Accessibility represents a board's physical environment and reachability.
+ */
 export enum Accessibility {
+  /**
+   * A public and freely accessible space.
+   */
   Public = "PUBLIC",
+
+  /**
+   * An accessible space but on private grounds.
+   */
   Private = "PRIVATE",
+
+  /**
+   * A hardly reachable or otherwise inaccessible (to the public) space.
+   */
   Inaccessible = "INACCESSIBLE"
 }
 
+/**
+ * Enum Material describes a board's surface material.
+ */
 export enum Material {
   Wood = "WOOD",
   Metal = "METAL",
@@ -12,16 +29,41 @@ export enum Material {
   Other = "OTHER"
 }
 
+/**
+ * Enum Size describes a boards's general physical size.
+ */
 export enum Size {
   Small = "SMALL",
   Medium = "MEDIUM",
   Large = "LARGE"
 }
 
+/**
+ * Enum Type represents a board's purpose.
+ */
 export enum Type {
+  /**
+   * An obituary for the deceased.
+   */
   Obituary = "OBITUARY",
+
+  /**
+   * A politics-oriented bulletin board.
+   */
   Politics = "POLITICS",
+
+  /**
+   * A board for notices and advisories.
+   */
   Notice = "NOTICE",
+
+  /**
+   * A board for the public with a variety of postable media.
+   */
   Public = "PUBLIC",
+
+  /**
+   * A board which does not fit in any of the other categories.
+   */
   Other = "OTHER"
 }
